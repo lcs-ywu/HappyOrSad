0a0f8e076c4eb61d1e3c8fbe5cdc182866f707d9
James Wu <ywu@lcs.on.ca>
2019-11-20 10:44:43 -0500

    Finish connecting storyboard and user interface

--- HappyOrSad/ViewController.swift
+++ HappyOrSad/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    // Outlets, constants, and variables defined here will be available for use anywhere below.
+    
+    @IBOutlet weak var userInput: UITextField!
+    
+    @IBOutlet weak var analyzeResult: UILabel!
+    
+    var happyScore = 0
+    var sadScore = 0
+    
+    
+    // MARK: Methods
+    // This method runs once when the view is loaded.
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func analyzeMood(_ sender: Any) {
+        
+        
+        
+        
+    }
+    
 }
 
